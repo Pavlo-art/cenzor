591470d3112b2f7c8b66aa8a251372a8c11fbf43
Pavlo-art <pavlosoroka07@gmail.com>
2021-03-31 22:29:23 +0300

    add format documents

--- script.js
+++ script.js
@@ -1,47 +1,35 @@
  let form = document.forms['form'];
  let span = document.querySelector(".badWords");
  console.log(span);
- let arr =[];
-form.add.onclick = function(){
-  if(form.badWordsInput.value.length>0){
-    let newWord = form.badWordsInput.value;
-    arr.push(newWord);
-    form.badWordsInput.value = "";
-    span.textContent = arr.join(",");
-  }
-  else{
-    form.badWordsInput.style.borderColor ="red";
-  }
- 
-}
+ let arr = [];
+ form.add.onclick = function () {
+   if (form.badWordsInput.value.length > 0) {
+     let newWord = form.badWordsInput.value;
+     arr.push(newWord);
+     form.badWordsInput.value = "";
+     span.textContent = arr.join(",");
+   } else {
+     form.badWordsInput.style.borderColor = "red";
+   }
 
-form.reset.onclick = function(){
- span.textContent = "";
- arr =[];
-}
+ }
 
-
-form.cenzor.addEventListener("click", function(){
-  let area = form.area.value;
-  let arrArea = area.split(" ");
-  console.log(arrArea);
-  for(i=0; i<arr.length; i++){
-    for(j=0; j<arrArea.length; j++){
-      if(arr[i] == arrArea[j]){
-        arrArea[j] = "*".repeat(arrArea[j].length);
-        form.area.value = arrArea.join(" ");
-      }
-    }
-  }
-})
+ form.reset.onclick = function () {
+   span.textContent = "";
+   arr = [];
+ }
 
 
-  
-
-
-
-
-
-
-
-
+ form.cenzor.addEventListener("click", function () {
+   let area = form.area.value;
+   let arrArea = area.split(" ");
+   console.log(arrArea);
+   for (i = 0; i < arr.length; i++) {
+     for (j = 0; j < arrArea.length; j++) {
+       if (arr[i] == arrArea[j]) {
+         arrArea[j] = "*".repeat(arrArea[j].length);
+         form.area.value = arrArea.join(" ");
+       }
+     }
+   }
+ })
